24ec79eee0b5f1d3cace8c666cc8789aed1edde4
lizyang95 <yy4@cs.cmu.edu>
2018-09-01 15:33:20 -0400

    sep1

--- leetcode5/longestSubstring.py
+++ leetcode5/longestSubstring.py
@@ -5,29 +5,33 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        chars = list(set(s))
-        memo = [[0]*len(chars)]
-        dic = {char:0 for char in s}
-        for char in s:
-            dic[char]+=1
-            memo.append([dic[char] for char in chars])
-        print(memo)
-        maxlen = 0
-        for i in range(len(memo)):
-            for j in range(i+1,len(memo)):
-                count = [memo[j][m] - memo[i][m] for m in range(len(memo[0]))]
-                if self.checkvalid(k,count) and j-i>maxlen:
-                    maxlen = j-i
-        return maxlen
+        for c in set(s):
+            if s.count(c) < k:
+                return max(self.longestSubstring(t, k) for t in s.split(c))
+        return len(s)
 
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        # Iterative approach:
+        stack = []
+        stack.append(s)
 
+        ans = 0
+        while stack:
+            s = stack.pop()
+            for c in set(s):
+                if s.count(c) < k:
+                    stack.extend([z for z in s.split(c)])
+                    break
+            else:
+                ans = max(ans, len(s))
 
-    def checkvalid(self,k,count):
-        for num in count:
-            if num < k and num != 0:
-                return False
-        return True
-
+        return ans 
 
 sol= Solution()
 s = "aaabb"
